6b30a36cca4242833655b9c99abe4ff699afc5f6
Leandrho <leandro.coder01@gmail.com>
2025-01-27 13:16:22 -0300

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+
+export type PersonalInfo = {
+    name: string,
+    email :string,
+    phone :string
+}
+export type Plan = {
+    name: string,
+    price: number
+    freeMonthsYearly: number
+    icon: string,
+}
+export type AddOn = {
+    name: string,
+    details: string,
+    price: number
+}
+
