5b69a59758723a7377469a835f6a9d603c955147
Leandrho <leandro.coder01@gmail.com>
2025-01-28 18:21:27 -0300

    add Personal info form

---
+++ src/components/PersonalInfoForm.tsx
@@ -0,0 +1,46 @@
+
+import { forwardRef, useEffect, useImperativeHandle } from "react"
+import { useMultiSteps } from "../hooks/useMultiSteps"
+
+type PersonalInfoFormProps = {
+
+}
+export const PersonalInfoForm = forwardRef(({  } :PersonalInfoFormProps, ref) => {
+//   const {personalInfo, setPersonInfo} = useMultiSteps();
+    const validateInputs = () :boolean => {
+        console.log('VALIDANDO PERSONAL INFO..');
+        return false;
+    }
+    useImperativeHandle(ref, ()=>({
+        validateInputs
+    }));
+        
+    return (
+        <div className="">        
+            <div className="">
+                <label htmlFor="person-name" className="text-sm">Name</label>{/*'need to use useID in the input field if you want to re-use the component..'*/}
+                <input id="person-name" 
+                    type="text" 
+                    placeholder="e.g. Stephen King" 
+                    className=" block w-full font-medium outline-hidden border rounded-sm px-4 py-2 "
+                />
+            </div>
+            <div className="">
+                <label htmlFor="person-name" className="text-sm">Email Address</label>{/*'need to use useID in the input field if you want to re-use the component..'*/}
+                <input id="person-name" 
+                    type="email" 
+                    placeholder="e.g. stephenking@lorem.com" 
+                    className=" block w-full font-medium outline-hidden border rounded-sm px-4 py-2 "
+                />
+            </div>
+            <div className="">
+                <label htmlFor="person-name" className="text-sm">Phone Number</label>{/*'need to use useID in the input field if you want to re-use the component..'*/}
+                <input id="person-name" 
+                    type="text" 
+                    placeholder="e.g. +1 234 567 890" 
+                    className=" block w-full font-medium outline-hidden border rounded-sm px-4 py-2"
+                />
+            </div>
+        </div>
+    )
+});
