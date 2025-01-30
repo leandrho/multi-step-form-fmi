1590952e8136cc21251a4fc193dc019ddccf2a78
Leandrho <leandro.coder01@gmail.com>
2025-01-30 10:51:12 -0300

    styles and more

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,7 +8,7 @@ type CardProps = {
 export const Card = ( { name, description, children } : CardProps ) => {
 
   return (
-    <div className="bg-white rounded-xl shadow-md px-6 py-8 mx-4 flex flex-col gap-2">
+    <div className="bg-white rounded-xl shadow-md md:shadow-none px-6 py-8 mx-4 flex flex-col gap-2 md:w-[450px] md:px-0">
         <h1 className="text-2xl font-bold">{name}</h1>
         <p className="text-base pb-2">{description}</p>   
         {children}
